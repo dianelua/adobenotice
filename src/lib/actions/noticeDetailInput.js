b90f66e11e1bbac7fa55be3798f2844ffd3a9d4b
Diane Jane Luague <diane@Dianes-MacBook-Air.local>
2017-10-27 13:54:55 +0800

    Update dependencies

--- src/lib/actions/noticeDetailInput.js
+++ src/lib/actions/noticeDetailInput.js
@@ -3,11 +3,12 @@
 module.exports = function(settings) {
     if (settings.domain) {
         var containerId = settings.container || "teconsent";
+        var gtm = settings.gtm ? "&gtm=1" : "";
         var notice = document.createElement("script");
         notice.setAttribute("async", "async");
         notice.setAttribute("type", "text/javascript");
         notice.setAttribute("crossorigin", "");
-        notice.src = "https://consent.truste.com/notice?domain=" + settings.domain + "&c=" + containerId;
+        notice.src = "https://consent.truste.com/notice?domain=" + settings.domain + "&c=" + containerId + gtm;
         (document.getElementsByTagName("body")[0] || document.getElementsByTagName("head")[0]).appendChild(notice);
     }
 };
