7d03102b440f81600c4e15cd9ec619986b453c28
Diane Jane Luague <diane@Dianes-MacBook-Air.local>
2017-12-07 18:36:20 +0800

    Adds text option functionality

--- src/lib/actions/noticeDetailInput.js
+++ src/lib/actions/noticeDetailInput.js
@@ -5,11 +5,13 @@ module.exports = function(settings) {
         var containerId = settings.container || "teconsent";
         var gtm = settings.gtm ? "&gtm=1" : "";
         var banner = settings.banner ? "&js=bb&noticeType=bb" : "";
+        var text = settings.text ? "&text=true" : "";
+        
         var notice = document.createElement("script");
         notice.setAttribute("async", "async");
         notice.setAttribute("type", "text/javascript");
         notice.setAttribute("crossorigin", "");
-        notice.src = "https://consent.truste.com/notice?domain=" + settings.domain + "&c=" + containerId + gtm + banner;
+        notice.src = "https://consent.truste.com/notice?domain=" + settings.domain + "&c=" + containerId + gtm + banner + text;
         (document.getElementsByTagName("body")[0] || document.getElementsByTagName("head")[0]).appendChild(notice);
     }
 };
