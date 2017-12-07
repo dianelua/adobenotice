0f1ae283fa71198ef0a82bc564480e4761853bba
Diane Jane Luague <diane@Dianes-MacBook-Air.local>
2017-12-07 15:31:38 +0800

    Adds banner functionality, formats html, ups version

--- src/lib/actions/noticeDetailInput.js
+++ src/lib/actions/noticeDetailInput.js
@@ -4,11 +4,12 @@ module.exports = function(settings) {
     if (settings.domain) {
         var containerId = settings.container || "teconsent";
         var gtm = settings.gtm ? "&gtm=1" : "";
+        var banner = settings.banner ? "&js=bb&noticeType=bb" : "";
         var notice = document.createElement("script");
         notice.setAttribute("async", "async");
         notice.setAttribute("type", "text/javascript");
         notice.setAttribute("crossorigin", "");
-        notice.src = "https://consent.truste.com/notice?domain=" + settings.domain + "&c=" + containerId + gtm;
+        notice.src = "https://consent.truste.com/notice?domain=" + settings.domain + "&c=" + containerId + gtm + banner;
         (document.getElementsByTagName("body")[0] || document.getElementsByTagName("head")[0]).appendChild(notice);
     }
 };
